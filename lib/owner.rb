ee7347ed27eb4de208e4b2d93b09ab30ed2a163b
isabelxklee <hello@isabelklee.co>
2020-03-15 20:44:48 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,94 @@
-class Owner
-  # code goes here
-end
+require 'pry'
+require_relative 'cat.rb'
+require_relative 'dog.rb'
+
+class Owner 
+
+  attr_reader :name, :species
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
+
+  def say_species 
+    "I am a #{species}."
+  end 
+
+  def self.all 
+    @@all
+  end 
+
+  def self.count
+    @@all.length
+  end 
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    owners_cats = Cat.all.select { |cat|
+      cat.owner == self
+    }
+    owners_cats
+  end
+
+  def dogs
+    owners_dogs = Dog.all.select { |dog|
+      dog.owner == self
+    }
+    owners_dogs
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end 
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end 
+
+  def walk_dogs
+    Dog.all.map { |dog|
+      dog.mood = "happy"
+    }
+  end 
+
+  def feed_cats
+    Cat.all.map { |cat|
+      cat.mood = "happy"
+    }
+  end 
+
+  def sell_pets
+    Cat.all.map { |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    }
+
+    Dog.all.map { |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    }
+  end 
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end 
+
+  def inspect
+    "<Owner: #{name}, #{species}>"
+  end 
+
+  # jon = Owner.new("Jon")
+  # gary = Owner.new("Gary")
+  # garfield = Cat.new("Garfield", jon)
+  # fido = Cat.new("Fido", jon)
+  # whiskers = Cat.new("Whiskers", gary)
+
+
+  # binding.pry
+end 
